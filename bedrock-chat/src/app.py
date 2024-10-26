a22a579f82fe1cf2bb2ad1569c08ae039c60c0b2
iamhansko <iamhansko@kakao.com>
2024-10-26 17:29:24 +0900

    fix: Minimize Model Tokens because of 5s Timeout

--- bedrock-chat/src/app.py
+++ bedrock-chat/src/app.py
@@ -1,23 +1,37 @@
 import json
-import boto3
+import os
+from langchain_aws import ChatBedrock
 
-def lambda_handler(event, context):  
-    request_body = json.loads(event['body'])
-    lambda_client = boto3.client('lambda')
-    lambda_client.invoke(
-        FunctionName="callback",
-        InvocationType='Event',
-        Payload=json.dumps({
-            'prompt' : request_body['userRequest']['utterance'],
-            'callback_url' : request_body['userRequest']['callbackUrl']
-        })
+# https://python.langchain.com/docs/integrations/llms/bedrock/
+def bedrock_chatbot(input_text):
+    bedrock_llm = ChatBedrock(
+        credentials_profile_name='default',
+        model_id='anthropic.claude-3-5-sonnet-20240620-v1:0',
+        model_kwargs={
+            "temperature": 0.5,
+            "max_tokens": 128
+        }
     )
+    return bedrock_llm.invoke([("human", input_text)])
 
+def lambda_handler(event, context):
+    request_body = json.loads(event['body'])
+    text_input = request_body['userRequest']['utterance']
+    response = bedrock_chatbot(text_input)
+
+    # https://kakaobusiness.gitbook.io/main/tool/chatbot/skill_guide/ai_chatbot_callback_guide#skillresponse
     return {
         'statusCode':200,
         'body': json.dumps({
             'version': '2.0',
-            'useCallback' : True,
-            'data': { 'text' : '' }
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": f"{response.content}"
+                        }
+                    }
+                ]
+            }
         })
     }
